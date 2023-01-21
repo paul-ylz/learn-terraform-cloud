0635e0233df4bb245bd197084282a08296e17729
Paul Yeoh <paulyeoh.lz@gmail.com>
2023-01-21 14:51:29 +0800

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,14 @@
 terraform {
 
+  /*
   cloud {
-    organization = "organization-name"
+    organization = "tutorgpybur"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+  */
 
   required_providers {
     aws = {
